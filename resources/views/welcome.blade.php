6f2187ca8437b4d142c8bcdeebf04d1e98809882
vagrant <vagrant@homestead>
2018-01-30 01:42:57 +0000

    部署到 Heroku

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel -- lilin
                 </div>
 
                 <div class="links">
