531d7c3e45a2e2fae41f0ebaa95aa52117664644
lilinphp@163.com <lilinphp@163.com>
2018-01-30 09:36:47 +0000

    signup

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,7 +28,7 @@ class User extends Authenticatable
     ];
 
 
-    public function gravater($size = '100')
+    public function gravatar($size = '100')
     {
         $hash = md5(strtolower(trim($this->ributton['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
